407016486d271b479a0f589bcee544f9fe07fcbb
NgocKhanh145 <nguyenngockhanh141105@gmail.com>
2025-04-13 09:20:38 +0700

    SX Phân hoạch

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+// Ý tưởng: Phân hoạch dãy ban đầu thành 3 phần: 
+// P1: Phần tử có giá trị bé hơn x (left)
+// P2: Phần tử có giá trị bằng x
+// P3: Phần tử có giá trị lớn hơn x (right)
 
 void QuickSort(int a[],int left, int right ){
     int i = left; int j = right;
-    int x = a[(left + right) / 2];
+    int x = a[(left + right) / 2]; // x lam moc 
     do
     {
-        while(a[i] < x) i++;
+        while(a[i] < x) i++; 
         while(a[j] > x) j--;
         if(i <= j ){
             swap(a[i], a[j]);
             i++; j--;
         }
     } while ( i <= j );
+    // Nếu i > j xét 2 TH này
     if (left < j)
     {
         QuickSort(a,left,j);
@@ -31,8 +36,5 @@ int main(){
     for (int i = 0; i < n; i++)
     {
         cout << mang[i] << " ";
-    }
-
-    
-    
+    }   
 }
